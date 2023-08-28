5fee4746cc3909197c149bf37b55fc32d3239784
francovinicius <duartevini.dev@gmail.com>
2023-08-28 10:20:51 -0300

    add fun to change imgs

--- app/src/main/java/com/lanterna/luminusapp/MainActivity.kt
+++ app/src/main/java/com/lanterna/luminusapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.lanterna.luminusapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.lanterna.luminusapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var estado = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.lanterna.setOnClickListener {
+
+            if(!estado) {
+                //setImage para alterar a imagem da lanterna
+                binding.lanterna.setImageResource(R.drawable.ic_lanterna_ligada)
+                estado = true
+            } else {
+                binding.lanterna.setImageResource(R.drawable.ic_lanterna_desligada)
+                estado = false
+            }
+        }
     }
 }
